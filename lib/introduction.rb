1a77df1a02c77c4c1fceca1b2d51ddefc9af9447
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-01 20:49:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,10 @@ def introduction(name)
 end
 introduction("Steve")
 
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language
 
 
 
